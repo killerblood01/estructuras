dd23d7da2491605fb70356e56d70820c0740a341
killerblood01 <30812826+killerblood01@users.noreply.github.com>
2017-08-28 21:28:24 -0500

    Create persona.java

---
+++ src/estructurasdedatos/cap1/tiposdedatos/persona.java
@@ -0,0 +1,47 @@
+	package estructurasdatos.cap1.tiposdedados;
+  
+  public class Persona{
+			
+			
+			private String nombre;
+            private String cedula;
+            private String telefono;
+            private String direccion;
+      
+      
+      
+      public String getNombre(){
+        
+        return nombre;
+                   
+     	
+		}
+      public void setNombre(String nombre){
+        this.nombre = nombre;
+      }
+      
+      public String getCedula(){
+        return cedula;
+      }
+      public void setCedula(String cedula){
+        this.cedula = cedula;
+      }
+      
+      public String getTelefono(){
+        return cedula;
+      }
+      public void setTelefono(String telefono){
+      this.telefono = telefono;
+      }
+      
+      public String getDireccion(){
+        return direccion;
+      }
+      
+      public void setDireccion(String direccion){
+      this.direccion = direccion;
+      }
+        
+    }
+        
+      
