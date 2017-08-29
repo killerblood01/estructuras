d0d7bef439f3c0331f10e9332d7f3becd8ee5235
killerblood01 <30812826+killerblood01@users.noreply.github.com>
2017-08-28 21:51:53 -0500

    Create figurasgeometricas.java

---
+++ src/estructurasdedatos/cap1/tiposdedatos/figurasgeometricas.java
@@ -0,0 +1,36 @@
+package estructurasdatos.cap1.tiposdedados;
+
+public class figurageometrica{
+  
+  
+  
+   
+   private  String codigo;
+   private  String tipo;
+   private  String nombre;
+  
+  
+  
+  public void getCodigo(String codigo){
+    return codigo;
+  }
+  
+  public String setCodigo(){
+    this.codigo = codigo;
+  }
+   
+  public void getTipo(String tipo)){
+    return tipo;
+  }
+  public String setTipo({
+    this.tipo = tipo;
+  }
+  
+  public void String getNombre(String nombre){
+    this.nombre = nombre;
+  }
+   public String setNombre(){
+    this.nombre = nombre;
+  }
+    
+}
