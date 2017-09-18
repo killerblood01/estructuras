82932e39b9f92e831e45cdb4a4c8359213c9ae48
jhonny <jhonyact@gmail.com>
2017-09-17 19:43:10 -0500

    agrego la lista y el nodo simples

--- src/estructurasdedatos/cap2/listas/ListaSimple.java
+++ src/estructurasdedatos/cap2/listas/ListaSimple.java
@@ -5,10 +5,85 @@
  */
 package estructurasdedatos.cap2.listas;
 
+import estructurasdedatos.utiles.nodos.NodoSimple;
+
 /**
  *
  * @author tusk
  */
 public class ListaSimple {
-    
+
+    private NodoSimple cabeza;
+    private NodoSimple cola;
+    private NodoSimple actual;
+
+    public void CrearListaSimple() {
+
+        this.cabeza = null;
+        this.cola = null;
+
+    }
+
+    public void insertar(int dato) {
+        NodoSimple nodo = new NodoSimple();
+        nodo.crearnodo();
+        nodo.setDato(dato);
+        if (this.cabeza == null && this.cola == null) {
+
+            this.cabeza = nodo;
+            this.cola = nodo;
+
+        } else {
+            cola.setSiguiente(nodo);
+            this.cola = nodo;
+
+        }
+    }
+
+    public void modificar(int dato) {
+        NodoSimple act = new NodoSimple();
+        act = cabeza;
+        int bandera = 0;
+        while (act != null) {
+            if (act.getDato() == dato) {
+                System.out.println("Ingrese nuevo dato para el nodo:");
+                //actual.setDato(actual.nextInt());
+                bandera = 1;
+
+            }
+            actual = actual.getSiguiente();
+        }
+        if (bandera == 0) {
+            System.out.println("Dato no esta en la lista");
+        }
+    }
+
+    public void eliminar(int dato) {
+        if (cabeza.getDato() == dato) {
+            cabeza = cabeza.getSiguiente();
+        } else {
+            NodoSimple aux = cabeza;
+            while (aux.getSiguiente().getDato() != dato) {
+                aux = aux.getSiguiente();
+            }
+            NodoSimple siguiente = aux.getSiguiente().getSiguiente();
+            aux.setSiguiente(siguiente);
+        }
+
+    }
+
+    public void listar() {
+
+        if (!(this.cabeza == null && this.cola == null)) {
+
+            NodoSimple aux = cabeza;
+
+            int i = 0;
+            while (aux != null) {
+                System.out.println("el dato buscado esta  :" + "[" + aux.getDato() + "]" + " " + "en la posicion " + "(" + i + ")" + "de la lista");
+                aux = aux.getSiguiente();
+                i++;
+            }
+        }
+    }
 }
--- src/estructurasdedatos/utiles/nodos/NodoSimple.java
+++ src/estructurasdedatos/utiles/nodos/NodoSimple.java
@@ -10,5 +10,33 @@ package estructurasdedatos.utiles.nodos;
  * @author tusk
  */
 public class NodoSimple {
+     private NodoSimple siguiente;
+    private int dato;
+
     
+    public NodoSimple  getSiguiente() {
+        return siguiente;
+    }
+
+  
+    public void setSiguiente(NodoSimple siguiente) {
+        this.siguiente = siguiente;
+    }
+
+  
+    public int getDato() {
+        return dato;
+    }
+
+ 
+    public void setDato(int dato) {
+        this.dato = dato;
+    }
+
+    public void crearnodo() {
+        this.siguiente = null;
+        this.dato = 0;
+
+    }
+
 }
