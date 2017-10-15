31d2e6fd4086af1d9cc475f716f79168ef7b50e7
jhonny <jhonyact@gmail.com>
2017-10-15 18:05:28 -0500

    agrego el diccioario y la tabla hash

---
+++ src/estructurasdedatos/cap3/tablahash/diccionario.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package estructurasdedatos.cap3.tablahash;
+
+/**
+ *
+ * @author jhony
+ */
+public class diccionario {
+    private int llave;
+    private String valor;
+
+    /**
+     * @return the llave
+     */
+    public int getLlave() {
+        return llave;
+    }
+
+    /**
+     * @param llave the llave to set
+     */
+    public void setLlave(int llave) {
+        this.llave = llave;
+    }
+
+    /**
+     * @return the valor
+     */
+    public String getValor() {
+        return valor;
+    }
+
+    /**
+     * @param valor the valor to set
+     */
+    public void setValor(String valor) {
+        this.valor = valor;
+    }
+    
+    
+}
+    
+
---
+++ src/estructurasdedatos/cap3/tablahash/tablahash.java
@@ -0,0 +1,85 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package estructurasdedatos.cap3.tablahash;
+
+/**
+ *
+ * @author jhony
+ */
+public class tablahash {
+    private diccionario[] tablahash;
+    private diccionario dato;
+    private int tam;
+
+    public void createVector(int t) {
+        this.tam = t;
+        this.tablahash = new diccionario[tam];
+    }
+
+    public int funcionhash(int valor) {
+        int vecht[] = new int[10];
+        int num = valor;
+        int pos;
+        for (int i = 0; i < 10; i++) {
+            int rest = num % 10;
+            num = num / 10;
+            vecht[i] = rest;
+        }
+        pos = vecht[0] + vecht[1] + vecht[2];
+        pos = pos * pos;
+        if (this.tablahash[pos] == null) {
+            return pos;
+        } else {
+            while (this.tablahash[pos] != null) {
+                if (pos < this.tam) {
+                    pos = pos + 1;
+                } else {
+                    pos = 0;
+                }
+            }
+            return pos;
+        }
+    }
+
+    public boolean insertar(int llave, String valor) {
+        diccionario nuevo = new diccionario();
+        nuevo.setLlave(llave);
+        nuevo.setValor(valor);
+        int posicion = this.funcionhash(llave);
+        this.tablahash[posicion] = nuevo;
+        return true;
+    }
+
+    public void listar() {
+
+        for (int i = 0; i < tam; i++) {
+            if (this.tablahash[i] != null) {
+                System.out.println("El dato " + this.tablahash[i].getLlave()
+                        + " esta en la posicion " + i + " de la tabla hash");
+            }
+        }
+    }
+
+    public void buscar(int dato) {
+        if (tablahash[1].getLlave() == dato) {
+            System.out.println("el dato " + dato + " esta en la posicion " + "1");
+        } else {
+            int i = 0;
+            while (i < tam) {
+                if (tablahash[1].getLlave() == dato) {
+                    System.out.println("el dato" + dato + "esta en la posicion " + i);
+                }
+                i++;
+            }
+        }
+    }
+
+    public void eliminarPorPosicion(int pos) {
+        tablahash[pos] = null;
+        System.out.println("el dato de la posicion " + pos + " ha sido eliminada");
+    }
+    
+}
