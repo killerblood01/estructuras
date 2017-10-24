41f5bb1a129fc4e1a66dc9121274f7f8f872ae90
jhonny <jhonyact@gmail.com>
2017-10-23 23:02:52 -0500

    agrego la base de arbol binario con orden preorden y post orden basico

--- src/estructurasdedatos/utiles/nodos/NodoArbolB.java
+++ src/estructurasdedatos/utiles/nodos/NodoArbolB.java
@@ -9,6 +9,57 @@ package estructurasdedatos.utiles.nodos;
  *
  * @author tusk
  */
-public class NodoArbolB {
-    
+        public class NodoArbolB {
+        private NodoArbolB hijoderecho;
+        private NodoArbolB hijoizquierdo;
+        private int dato;
+
+        public void NodoArbol() {
+            this.setHijoderecho(null);
+            this.setHijoizquierdo(null);
+            this.setDato(0);
+        }
+
+    /**
+     * @return the hijoderecho
+     */
+    public NodoArbolB getHijoderecho() {
+        return hijoderecho;
+    }
+
+    /**
+     * @param hijoderecho the hijoderecho to set
+     */
+    public void setHijoderecho(NodoArbolB hijoderecho) {
+        this.hijoderecho = hijoderecho;
+    }
+
+    /**
+     * @return the hijoizquierdo
+     */
+    public NodoArbolB getHijoizquierdo() {
+        return hijoizquierdo;
+    }
+
+    /**
+     * @param hijoizquierdo the hijoizquierdo to set
+     */
+    public void setHijoizquierdo(NodoArbolB hijoizquierdo) {
+        this.hijoizquierdo = hijoizquierdo;
+    }
+
+    /**
+     * @return the dato
+     */
+    public int getDato() {
+        return dato;
+    }
+
+    /**
+     * @param dato the dato to set
+     */
+    public void setDato(int dato) {
+        this.dato = dato;
+    }
 }
+
